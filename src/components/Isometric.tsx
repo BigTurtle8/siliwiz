2a800e3daa87a64010b9fdd38080375350ff29f3
Marcus Alagar <mvala079@gmail.com>
2025-09-26 17:58:31 -0700

    feat: separately, manually handled mesh
    Moved JSX initialization of full mesh to
    a separate component, `SiliconMesh`, so that
    hopefully can do boolean operations on the separate
    meshes to fix their z-fighting.
    
    Also ended up, for `SiliconMesh`, subverting the normal `Entity`
    pattern and directly implementing the adding of the meshes to the
    scene using `three`. As of the time of development `solid-three`'s
    Entity wasn't updating, even when `createEffect`'s callback
    was being called and updating `group`. This is probably my
    own misunderstanding, but got to work for now by essentially doing
    the scene management directly.

--- src/components/Isometric.tsx
+++ src/components/Isometric.tsx
@@ -1,51 +1,72 @@
 // SPDX-License-Identifier: Apache-2.0
 
-import { For } from 'solid-js';
-import { Canvas, Entity } from 'solid-three';
+import { createEffect, onMount } from 'solid-js';
+import { autodispose, Canvas, Entity, useThree } from 'solid-three';
 import * as THREE from 'three';
 import { layout, rectLayer, rectViaLayer } from '../model/layout';
 
-export default function Isometric() {
+const SiliconMesh = () => {
   const rects = () => layout.rects;
 
+  let group: THREE.Group | undefined;
+
+  onMount(() => {
+    createEffect(() => {
+      // Had to circumvent `solid-three`'s intended
+      // `Entity` pattern, as upon developing (10/2/2025),
+      // despite `createEffect` properly running when
+      // dependency `rects()` changed, `<Entity from={group} />`
+      // wasn't correctly updating for whatever reason.
+      const { scene } = useThree();
+
+      scene.remove(group);
+
+      group = autodispose(new THREE.Group());
+
+      for (const rect of rects()) {
+        const layer = rectLayer(rect);
+        const viaLayer = rectViaLayer(layout, rect);
+        if (!viaLayer || !layer) {
+          continue;
+        }
+
+        const geometry = autodispose(
+          new THREE.BoxGeometry(rect.width, rect.height, viaLayer.crossHeight),
+        );
+        const material = autodispose(new THREE.MeshStandardMaterial({ color: layer.color }));
+
+        const mesh = autodispose(new THREE.Mesh(geometry, material));
+        mesh.position.set(
+          rect.x + rect.width / 2,
+          -(rect.y + rect.height / 2),
+          -viaLayer.crossY - viaLayer.crossHeight / 2,
+        );
+
+        mesh.updateMatrix();
+
+        group.add(mesh);
+      }
+
+      group.rotation.set((7 * Math.PI) / 4, 0, Math.PI / 4);
+      group.position.set(-275, 100, 0);
+
+      group.updateMatrix();
+
+      scene.add(group);
+    });
+  });
+
+  return null;
+};
+
+export default function Isometric() {
   return (
     <Canvas
       defaultCamera={{ position: [0, 0, 900] }}
       shadows
       style={{ width: '400px', height: '600px' }}
     >
-      <Entity
-        from={THREE.Group}
-        position={[-275, 100, 0]}
-        rotation={[(7 * Math.PI) / 4, 0, Math.PI / 4]}
-      >
-        <For each={rects()}>
-          {(rect) => {
-            const layer = rectLayer(rect);
-            const viaLayer = rectViaLayer(layout, rect);
-            if (!viaLayer || !layer) {
-              return <Entity from={THREE.Mesh} />;
-            }
-
-            return (
-              <Entity
-                from={THREE.Mesh}
-                position={[
-                  rect.x + rect.width / 2,
-                  -(rect.y + rect.height / 2),
-                  -viaLayer.crossY - viaLayer.crossHeight / 2,
-                ]}
-              >
-                <Entity
-                  from={THREE.BoxGeometry}
-                  args={[rect.width, rect.height, viaLayer.crossHeight]}
-                />
-                <Entity from={THREE.MeshStandardMaterial} color={layer.color} />
-              </Entity>
-            );
-          }}
-        </For>
-      </Entity>
+      <SiliconMesh />
       <Entity from={THREE.AmbientLight} intensity={0.1} />
       <Entity from={THREE.PointLight} position={[0, 600, -250]} />
     </Canvas>
